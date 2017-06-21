d39dc8f3668c4f6f30a7c3d4b85cf7d070378074
Cassio Ribeiro <cassior16@gmail.com>
2017-06-21 18:36:23 -0300

    Create Routers

--- modules/Router/Router.php
+++ modules/Router/Router.php
@@ -1,48 +1,81 @@
 <?php
-class Router{
+class Router {
 
+	private $core;
 	private $get;
 	private $post;
 
-	private function __construct(){
+	private function __construct() {}
 
-	}
-
-	//carregando apenas uma instancia, pagrao Singleton
-	public static function getInstance(){
+	public static function getInstance() {
 		static $inst = null;
-		if($inst === null){
+		if($inst === null) {
 			$inst = new Router();
 		}
 		return $inst;
 	}
 
-	//carregando as rotas
-	public function load(){
+	public function load() {
+		$this->core = Core::getInstance();
 		$this->loadRouteFile('default');
-
 		return $this;
 	}
 
-	//carregando um arquivo pela roda
-	public function loadRouteFile($f){
-		if(file_exists('routes/'.$f.'.php')){
-			return 'routes/'.$f.'.php';
+	public function loadRouteFile($f) {
+		if(file_exists('routes/'.$f.'.php')) {
+			require 'routes/'.$f.'.php';
 		}
 	}
 
-	//processando as rotas
-	public function match(){
+	public function match() {
+		$url = ((isset($_GET['url']))?$_GET['url']:'');
+
+		switch($_SERVER['REQUEST_METHOD']) {
+			case 'GET':
+			default:
+				$type = $this->get;
+				break;
+			case 'POST':
+				$type = $this->post;
+				break;
+		}
+
+		// Loop em todos os routes
+		foreach($type as $pt => $func) {
+			// Identifica os argumentos e substitui por regex (expressao regular)
+			$pattern = preg_replace('(\{[a-z0-9]{0,}\})', '([a-z0-9]{0,})', $pt);
+
+			// Faz o match de URL,ve se a URL bateu com a ER
+			if(preg_match('#^('.$pattern.')*$#i', $url, $matches) === 1){
+				array_shift($matches);//apagando o primeiro elemento
+				array_shift($matches);
+				
+				//Pega todos os argumentos para associas
+				$item = array();
+				if(preg_match_all('(\{[a-z0-9]{0,}\})', $pt, $m)){
+					$itens = preg_replace('(\{|\})', '', $m[0]);
+				}
+
+				//Faz a associacao
+				$arg = array();
+				foreach ($matches as $key => $match) {
+					$arg[$itens[$key]] = $match;
+				}
+
+				$func($arg);//chama a funcao
+
+				break;//para o loop
+			}		
+			
+		}
 
 	}
 
-	//pegando dado pelo metodo get pela URL
-	public function get($pattern, $function){
+	public function get($pattern, $function) {
 		$this->get[$pattern] = $function;
 	}
 
-	//pegando dado pelo metodo oculto post
-	public function post(){
+	public function post($pattern, $function) {
 		$this->post[$pattern] = $function;
 	}
 
@@ -54,6 +87,13 @@ class Router{
 
 
 
-}
 
+
+
+
+
+
+
+
+}
 ?>
--- routes/noticias.php
+++ routes/noticias.php
@@ -6,3 +6,7 @@ $this->get('noticias', function($arg){
 $this->get('noticias/{id}', function($arg){
 	echo 'quero ver uma noticia especifica...';
 });
+
+$this->get('nome/{nome}/{idade}', function($arg){
+	echo "Meu nome: ".$arg['nome']." e tenho ".$arg['idade']." anos";
+});
