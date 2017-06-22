b00642bc82eb9e044cf5b7a268661ef602561005
Cassio Ribeiro <cassior16@gmail.com>
2017-06-21 21:33:06 -0300

    sql db

---
+++ micro_framework_noticias.sql
@@ -0,0 +1,5 @@
+INSERT INTO micro_framework.noticias (titulo, corpo) VALUES ('teste', 'corpo da mensagem
+
+');
+INSERT INTO micro_framework.noticias (titulo, corpo) VALUES ('teste2', 'corpo da mensagem2');
+INSERT INTO micro_framework.noticias (titulo, corpo) VALUES ('teste3', 'corpo da mensagem3');
